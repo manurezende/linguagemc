fedfb6c4e155d35b01927e29ebd384131570cb44
manuella rezende <manurezendelourenco@gmail.com>
2024-09-10 14:39:25 +0000

    inicio do estudo em C

---
+++ primeiro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int num;
+    printf ("digite um valor inteiro e ENTER\n");
+    scanf("%d", &num);
+    printf("o valor digitado é %d\n", num);
+    return 0;
+
+
+
+}
