f9c82600a31445063b8c3e36e79d11c17b7c5660
manuella rezende <manurezendelourenco@gmail.com>
2024-09-12 14:59:13 +0000

    trabalhando co while, contagem de 10 a 0

---
+++ contagem.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int valor;
+    valor = 100;
+    while (valor >= 0)                 // ele vai ficar repetindo enquanto for verdade
+    {
+       printf("%d\n",valor);
+       valor--;                         // incrementa uma unica unidade no valor da variavel
+    }
+    
+    return 0;
+
+
+}
